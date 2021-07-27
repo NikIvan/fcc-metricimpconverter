6541ad99db7feea29f057d73bfef4a0ad27ded28
Mykyta Ivanchenko <mivansevdon@gmail.com>
2021-07-27 04:58:14 +0300

    Fix tests to pass fcc tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,82 +6,80 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-  suite('Integration tests with chai-http', () => {
-    test('Convert a valid input such as 10L', (done) => {
-      const input = '10L';
+  test('Convert a valid input such as 10L', (done) => {
+    const input = '10L';
 
-      chai
-        .request(server)
-        .get(`/api/convert?input=${input}`)
-        .end((err, res) => {
-          assert.strictEqual(res.status, 200);
-          assert.strictEqual(res.type, 'application/json');
-          assert.strictEqual(res.body.initNum, 10);
-          assert.strictEqual(res.body.initUnit, 'L');
-          assert.strictEqual(res.body.returnNum, 2.64172);
-          assert.strictEqual(res.body.returnUnit, 'gal');
-          assert.strictEqual(res.body.string, '10 liters converts to 2.64172 gallons');
+    chai
+      .request(server)
+      .get(`/api/convert?input=${input}`)
+      .end((err, res) => {
+        assert.strictEqual(res.status, 200);
+        assert.strictEqual(res.type, 'application/json');
+        assert.strictEqual(res.body.initNum, 10);
+        assert.strictEqual(res.body.initUnit, 'L');
+        assert.strictEqual(res.body.returnNum, 2.64172);
+        assert.strictEqual(res.body.returnUnit, 'gal');
+        assert.strictEqual(res.body.string, '10 liters converts to 2.64172 gallons');
 
-          done();
-        });
-    });
+        done();
+      });
+  });
 
-    test('Convert an invalid input such as 32g', (done) => {
-      const input = '32g';
+  test('Convert an invalid input such as 32g', (done) => {
+    const input = '32g';
 
-      chai
-        .request(server)
-        .get(`/api/convert?input=${input}`)
-        .end((err, res) => {
-          assert.strictEqual(res.status, 200, input);
-          assert.strictEqual(res.text, 'invalid unit', input);
-          done();
-        });
-    });
+    chai
+      .request(server)
+      .get(`/api/convert?input=${input}`)
+      .end((err, res) => {
+        assert.strictEqual(res.status, 200, input);
+        assert.strictEqual(res.text, 'invalid unit', input);
+        done();
+      });
+  });
 
-    test('Convert an invalid number such as 3/7.2/4kg', (done) => {
-      const input = '3/7.2/4kg';
+  test('Convert an invalid number such as 3/7.2/4kg', (done) => {
+    const input = '3/7.2/4kg';
 
-      chai
-        .request(server)
-        .get(`/api/convert?input=${input}`)
-        .end((err, res) => {
-          assert.strictEqual(res.status, 200, input);
-          assert.strictEqual(res.text, 'invalid number', input);
-          done();
-        });
-    });
+    chai
+      .request(server)
+      .get(`/api/convert?input=${input}`)
+      .end((err, res) => {
+        assert.strictEqual(res.status, 200, input);
+        assert.strictEqual(res.text, 'invalid number', input);
+        done();
+      });
+  });
 
-    test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram', (done) => {
-      const input = '3/7.2/4kilomegagram';
+  test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram', (done) => {
+    const input = '3/7.2/4kilomegagram';
 
-      chai
-        .request(server)
-        .get(`/api/convert?input=${input}`)
-        .end((err, res) => {
-          assert.strictEqual(res.status, 200, input);
-          assert.strictEqual(res.text, 'invalid number and unit', input);
-          done();
-        });
-    });
+    chai
+      .request(server)
+      .get(`/api/convert?input=${input}`)
+      .end((err, res) => {
+        assert.strictEqual(res.status, 200, input);
+        assert.strictEqual(res.text, 'invalid number and unit', input);
+        done();
+      });
+  });
 
-    test('Convert with no number such as kg', (done) => {
-      const input = 'kg';
+  test('Convert with no number such as kg', (done) => {
+    const input = 'kg';
 
-      chai
-        .request(server)
-        .get(`/api/convert?input=${input}`)
-        .end((err, res) => {
-          assert.strictEqual(res.status, 200);
-          assert.strictEqual(res.type, 'application/json');
-          assert.strictEqual(res.body.initNum, 1);
-          assert.strictEqual(res.body.initUnit, 'kg');
-          assert.strictEqual(res.body.returnNum, 2.20462);
-          assert.strictEqual(res.body.returnUnit, 'lbs');
-          assert.strictEqual(res.body.string, '1 kilograms converts to 2.20462 pounds');
+    chai
+      .request(server)
+      .get(`/api/convert?input=${input}`)
+      .end((err, res) => {
+        assert.strictEqual(res.status, 200);
+        assert.strictEqual(res.type, 'application/json');
+        assert.strictEqual(res.body.initNum, 1);
+        assert.strictEqual(res.body.initUnit, 'kg');
+        assert.strictEqual(res.body.returnNum, 2.20462);
+        assert.strictEqual(res.body.returnUnit, 'lbs');
+        assert.strictEqual(res.body.string, '1 kilograms converts to 2.20462 pounds');
 
-          done();
-        });
-    });
+        done();
+      });
   });
 });
